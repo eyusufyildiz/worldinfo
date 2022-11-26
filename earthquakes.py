8e14ff85f3fa094182020875f7af81f44f7924d6
eyusufyildiz <eyusufyildiz@gmail.com>
2022-11-26 02:58:56 -0800

    Update earthquakes.py

--- earthquakes.py
+++ earthquakes.py
@@ -33,4 +33,4 @@ st.write(f"Number of earthquakes (>4.5) in last week: {len(quakes)}")
 
 sorted_quakes = quakes.sort_values(by=['mag'], ascending=False)
 #st.line_chart(sorted_quakes)
-st.print(sorted_quakes)
+st.write(sorted_quakes)
