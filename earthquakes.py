1a8c0ce5d212fd6b1e7d0b28fc73e077fa42fcbf
eyusufyildiz <eyusufyildiz@gmail.com>
2022-11-26 03:04:55 -0800

    Update earthquakes.py

--- earthquakes.py
+++ earthquakes.py
@@ -27,10 +27,21 @@ for q in res :
     else: x['color'] = 'green'
 
 quakes = pd.json_normalize(quakes)
+fig = px.scatter_mapbox(quakes, lat="lat", lon="lon", 
+                        title= "Number of earthquakes (>4.5) in last week:",
+                        hover_name="place", size="mag", 
+                        #animation_frame = 'time', animation_group = 'place', 
+                        #color_continuous_scale=px.colors.cyclical.HSV,
+                        color_continuous_scale=px.colors.sequential.YlOrRd,
+                        hover_data=['title', "mag", "depth", "time", 'alert', "type", "tsunami"],
+                        color_discrete_sequence=[quakes.color], zoom=2, height=500, color='mag')
+                        #color_discrete_sequence=[quakes.color], zoom=2, height=500)
+fig.update_layout(mapbox_style="stamen-terrain")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+#fig.update_layout(autosize=True, width=1500, height=700)
 
-st.write(f"Number of earthquakes (>4.5) in last week: {len(quakes)}")
-
+st.markdown(f"Number of earthquakes (>4.5) in last week: ** {len(quakes)} **")
+st.pyplot(fig )
 
 sorted_quakes = quakes.sort_values(by=['mag'], ascending=False)
-#st.line_chart(sorted_quakes)
 st.write(sorted_quakes)
