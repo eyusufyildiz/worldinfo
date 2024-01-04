425326690e8391726c3027c1345e32f630b8dd25
eyusufyildiz <eyusufyildiz@gmail.com>
2024-01-03 22:42:49 -0800

    Update StarlinkGps.py

--- mypages/StarlinkGps.py
+++ mypages/StarlinkGps.py
@@ -6,6 +6,9 @@ from utils import tools as tool
 
 def satellites():
     st.container()
-    sat = '<iframe src="https://satellitemap.space/" width="800px" height="600px" frameborder="0" title="Starlink Satellite Map"></iframe>'
-    components.html(sat, height=800)
+    #sat = '<iframe src="https://satellitemap.space/" width="800px" height="600px" frameborder="0" title="Starlink Satellite Map"></iframe>'
+    #components.html(sat, height=800)
+    
+    sat = '<iframe src="https://satellitemap.space/"  frameborder="0" title="Starlink Satellite Map"></iframe>'
+    components.html(sat)
 
