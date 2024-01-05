b9e9eec384596d2d0caf055e5e55fea1abdafa9f
eyusufyildiz <eyusufyildiz@gmail.com>
2024-01-04 22:18:23 -0800

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -58,7 +58,7 @@ def plotly_map(data, title, hover_name=None, hover_data=None,
                             # color_discrete_sequence="red",
                             zoom=zoom, height=height)
         #fig.update_layout(mapbox_style="mapbox_style")  # "open-street-map"
-        fig.update_layout(mapbox_style="satellite")  # "open-street-map"
+        fig.update_layout(mapbox_style="stamen-terrain")  # "open-street-map"
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
         return fig
 
