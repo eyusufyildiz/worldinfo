af972790f4bd09222c3c8f9296ce7d05cb141414
eyusufyildiz <eyusufyildiz@gmail.com>
2024-04-22 00:41:13 -0700

    Update Satellites.py

--- mypages/Satellites.py
+++ mypages/Satellites.py
@@ -5,7 +5,7 @@ from utils import tools as tool
 #tool.streamlit_config(page_title="📡 Starlink / GPS Satellites", page_icon="📡")
 
 def satellites():
-    st.audio("Audio.mpeg", format="audio/mpeg", loop=True)
+    st.audio("mypages/Audio.mpeg", format="audio/mpeg", loop=True)
     st.container()
     sat = '<iframe src="https://satellitemap.space/" width="800px" height="600px" frameborder="0" title="Starlink Satellite Map"></iframe>'
     components.html(sat, height=600)
