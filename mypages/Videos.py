b94d533c6ae5e769b08011f5775909f93e2df33c
eyusufyildiz <eyusufyildiz@gmail.com>
2024-11-14 01:23:46 -0800

    Update Videos.py

--- mypages/Videos.py
+++ mypages/Videos.py
@@ -5,6 +5,7 @@ from utils import tools as tool
 #tool.streamlit_config(page_title="📡 Starlink / GPS Satellites", page_icon="📡")
 
 def music_video():
-    VIDEO_URL = "https://youtu.be/tW4KBk_JL7M"
-    st.video(VIDEO_URL)
+    st.video("https://youtu.be/tW4KBk_JL7M")
     st.audio("mypages/Audio.mpeg", format="audio/mpeg", loop=True)
+    st.audio("mypages/mzk/Altan Urag - No Mercy [ ezmp3.cc ].mp3", format="audio/mpeg", loop=True)
+    st.audio("mypages/mzk/AÇerkes Türküsü _ Ağlatan Qafe _ Hüzünlendiren Müzikler Serisi _ Circassian Crying CAFE [ ezmp3.cc ].mp3", format="audio/mpeg", loop=True)
