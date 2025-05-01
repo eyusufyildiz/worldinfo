cb729cc8b810f5dd3bb9ead616bfe6a5278bdf73
eyusufyildiz <eyusufyildiz@gmail.com>
2025-05-01 16:53:35 -0700

    Update Volcanos.py

--- mypages/Volcanos.py
+++ mypages/Volcanos.py
@@ -11,7 +11,7 @@ def http_requests(url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     return requests.get(url, headers=headers)
 
-def get_volcanos():
+def _get_volcanos():
     url1="https://volcano.oregonstate.edu/volcano_table"
     url2="https://volcano.oregonstate.edu/volcano_table?page=1"
     url3="https://volcano.oregonstate.edu/volcano_table?page=2"
@@ -25,6 +25,22 @@ def get_volcanos():
     st.write(f"Number of volcanos 🌋: {len(volcanos)} from {url1}")
 
     fig = tool.plotly_map(volcanos, lat="Latitude (dd)", lon="Longitude (dd)", 
+                        title= "Volcanos", hover_name="Volcano Name", 
+                        hover_data=["Country",  "Elevation (m)"])
+
+    st.write(fig)
+
+    with st.expander("Volcano list"):
+        st.dataframe(volcanos)
+
+
+
+
+def _get_volcanos():
+    volcanos = pd.read_csv('GVP_Volcano_List_Holocene_202505011922.csv')
+    st.write(f"Number of volcanos 🌋: {len(volcanos)} from {url1}")
+
+    fig = tool.plotly_map(volcanos, lat="Latitude", lon="Longitude", 
                         title= "Volcanos", hover_name="Volcano Name", 
                         hover_data=["Country",  "Elevation (m)"])
 
