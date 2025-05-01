543ac025c65d92f955d9456e791e18f8642a1e15
eyusufyildiz <eyusufyildiz@gmail.com>
2025-05-01 16:03:28 -0700

    Update Volcanos.py

--- mypages/Volcanos.py
+++ mypages/Volcanos.py
@@ -7,7 +7,7 @@ from utils import tools as tool
 
 #tool.streamlit_config(page_title="🌋 Volcanos", page_icon="🌋")
 
-def htt_requests(url):
+def http_requests(url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     return requests.get(url, headers=headers)
 
@@ -16,9 +16,9 @@ def get_volcanos():
     url2="https://volcano.oregonstate.edu/volcano_table?page=1"
     url3="https://volcano.oregonstate.edu/volcano_table?page=2"
     volcanos = pd.DataFrame()
-    df1 = pd.read_html(htt_requests(url1))[0]
-    df2 = pd.read_html(htt_requests(url2))[0]
-    df3 = pd.read_html(htt_requests(url3))[0]
+    df1 = pd.read_html(http_requests(url1).text)[0]
+    df2 = pd.read_html(http_requests(url2).text)[0]
+    df3 = pd.read_html(http_requests(url3).text)[0]
 
     volcanos = pd.concat([df1, df2, df3], ignore_index=True)
     #volnanos = volnanos.sort_values(by=['Country'])
