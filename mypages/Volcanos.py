9ed2a6ec917e6e739ea2ac71c89298fe6c3f8867
eyusufyildiz <eyusufyildiz@gmail.com>
2025-05-01 16:00:11 -0700

    Update Volcanos.py

--- mypages/Volcanos.py
+++ mypages/Volcanos.py
@@ -1,19 +1,24 @@
 # https://github.com/pahlisch/streamlit-app-volcano
 import pandas as pd
+import requests
 import streamlit as st
 import plotly.express as px
 from utils import tools as tool
 
 #tool.streamlit_config(page_title="🌋 Volcanos", page_icon="🌋")
 
+def htt_requests(url):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    return requests.get(url, headers=headers)
+
 def get_volcanos():
     url1="https://volcano.oregonstate.edu/volcano_table"
     url2="https://volcano.oregonstate.edu/volcano_table?page=1"
     url3="https://volcano.oregonstate.edu/volcano_table?page=2"
     volcanos = pd.DataFrame()
-    df1 = pd.read_html(url1)[0]
-    df2 = pd.read_html(url2)[0]
-    df3 = pd.read_html(url3)[0]
+    df1 = pd.read_html(htt_requests(url1))[0]
+    df2 = pd.read_html(htt_requests(url2))[0]
+    df3 = pd.read_html(htt_requests(url3))[0]
 
     volcanos = pd.concat([df1, df2, df3], ignore_index=True)
     #volnanos = volnanos.sort_values(by=['Country'])
