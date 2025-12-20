015248dae418cca4303a63cdb558ea2789b16ae4
eyusufyildiz <eyusufyildiz@gmail.com>
2025-12-20 01:27:22 -0800

    Update ObjectDetection.py

--- mypages/ObjectDetection.py
+++ mypages/ObjectDetection.py
@@ -22,7 +22,7 @@ def get_stream_url(youtube_url: str) -> str:
     """
     ydl_opts = {
         "quiet": True,
-        'cookies': 'cookies.txt',
+        'cookies': '/tmp/cookies.txt',
         "format": "best",
         "noplaylist": True,
         "live_from_start": True,
